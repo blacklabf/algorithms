4755476d3bad2c246b257827bcec96459669bf3d
방소영 <bjo8244@naver.com>
2024-03-06 11:09:11 +0900

    [240306] LeetCode_TwoSum

---
+++ DataStructure/LC_1_TwoSum.java
@@ -0,0 +1,30 @@
+package DataStructure;
+
+import java.util.Arrays;
+
+public class LC_1_TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int[] ans = new int[2];
+
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;   
+    }
+
+    // test
+    public static void main(String[] args) {
+        int[] input = {2,7,11,15};
+        int target = 9;
+        int[] ans = twoSum(input, target);
+        System.out.println(Arrays.toString(ans));
+    }
+}
