62c4e93fc03b1727ab93571ca0c6b873bdfb7d8f
방소영 <bjo8244@naver.com>
2024-03-10 15:25:29 +0900

    Programmers_기능개발_reference

--- java-solved/programmers/PG_기능개발.java
+++ java-solved/programmers/PG_기능개발.java
@@ -1,40 +1,17 @@
-import java.util.ArrayDeque;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.Arrays;
 class PG_기능개발{
-    public static LinkedList<Integer> solution(int[] progresses, int[] speeds) {
-
-        LinkedList<Integer> answer = new LinkedList<>();
-        Queue<Integer> q1 = new ArrayDeque<>();
-        Queue<Integer> q2 = new ArrayDeque<>();
-        int N = progresses.length;
-
-        for (int i = 0; i < N; i++) {
-            q1.add(progresses[i]);
-            q2.add(speeds[i]);
-        }
-
-        int day = 1;
-        int cnt = 1;
-
-        while (!q1.isEmpty()) {
-            int a1 = q1.poll();
-            int a2 = q2.poll();
-            if (a1 + a2 * day < 100) {
-                answer.add(cnt);
-                if ((100 - a1) % a2 != 0) {
-                    day = (100 - a1) / a2 + 1;
-                } else {
-                    day = (100 - a1) / a2;
-                }
-                cnt = 1;
-            } else {
-                cnt++;
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
             }
+            dayOfend[day]++;
         }
-        return answer;
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
+    
 
     public static void main(String[] args) {
         int[] progresses = {93,30,55};
