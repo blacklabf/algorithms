c4c4ae4b955792e5d1f92e8e514cb0ec070c9c20
방소영 <bjo8244@naver.com>
2024-06-29 21:53:02 +0900

    [240629] 리팩토링
    1. Character.getNumericValue 사용
    2. nums[6] 짝홀수 나누지 않고 연산

--- java-solved/baekjoon/BOJ_1465_방번호.java
+++ java-solved/baekjoon/BOJ_1465_방번호.java
@@ -3,48 +3,27 @@ import java.util.Arrays;
 
 public class BOJ_1465_방번호 {
     
-    // 한 세트 0~9 숫자가 하나씩
-    // 다솜 방번호 주어짐 -> 필요한 세트의 개수
-    // 6 = 9
-    // 방번호 N <= 1,000,000 이 주어질 떄, 필요한 갯수 출력
-
-
-    // N이 쉼표까지 주어졌을 떄, 고려
-    // charAt(), toCharArray(), 문자열 길이, String.valueOf()
     static String N;
     static int[] nums;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = br.readLine();
-        // 0부터 9까지 10개의 배열을 만들고 idx 별로 숫자 있을 때 추가, 6이나 9는 모두 6에 추가
-        nums = new int[10];
-        for(int i=0; i<N.length(); i++){
-            int tmp = Integer.parseInt(String.valueOf(N.charAt(i)));
-            if(tmp == 6 || tmp == 9){
-                nums[6]++;
-            }else{
-                nums[tmp]++;
-            }
+        // nums[9]가 필요 없어서 0부터 8까지 크기 9의 배열 선언
+        nums = new int[9]; 
+
+        for (int i = 0; i < N.length(); i++) {
+            int tmp = Character.getNumericValue(N.charAt(i));
+            if(tmp==9) {nums[6]++;}
+            else{nums[tmp]++;}
         }
 
-        // 최댓값을 찾고 만약 그게 6에 해당하는 거라면 두번쨰 최댓값과 나누기 2한 값을 찾아봄 
-        // -> 저렇게 하지 말고 6의 값을 일단 2로 나눈 다음에 최댓값 비교 
+        // 6과 9는 같이 사용 가능하므로 처리 : 홀수일 떄, 짝수일 때 상관없이 +1 후, 몫으로 계산
+        nums[6] = (nums[6] + 1) / 2; // 최소 6과 9의 세트 개수
 
-        if(nums[6] % 2 == 0) {
-            nums[6] = nums[6] / 2;
-        }else{
-            nums[6] = (nums[6]+1) /2 ;
-        }
-
+        // 6과 9를 제외한 다른 숫자들의 최대 세트 개수 계산
+        // StreamAPI 사용
         int ans = Arrays.stream(nums).max().orElseThrow();
         System.out.println(ans);
-
-        
-
-        
-
-
-        
     }
 }
